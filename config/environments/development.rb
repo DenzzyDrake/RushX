99cff46aa1035e6d1a9c3b5bfc702c7777bc4064
Denzel Carter <carter.denzel@yahoo.com>
2014-09-25 15:13:18 -0400

    use senddrill

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,9 +8,9 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'http://rushalertx.herokuapp.com' }
 
   config.action_mailer.smtp_settings = {
-   address: "smtp.gmail.com",
+   address: "smtp.sendgrid.net",
    port: "587",
-   domain: "gmail.com",
+   domain: 'heroku.com',
    authentication: "plain",
    enable_starttls_auto: true,
    user_name: ENV["GMAIL_USERNAME"],
