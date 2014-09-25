06d70a74c49ef68861a8ecba280c62bfd42b542e
Denzel Carter <carter.denzel@yahoo.com>
2014-09-25 01:03:44 -0400

    fixed an error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140925035744) do
+ActiveRecord::Schema.define(version: 20140925044322) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -52,6 +52,11 @@ ActiveRecord::Schema.define(version: 20140925035744) do
     t.date     "published_at"
     t.text     "content"
     t.string   "picture"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "contacts", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
